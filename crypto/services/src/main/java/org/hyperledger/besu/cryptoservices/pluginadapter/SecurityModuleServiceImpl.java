fd8389c576f4065ec77fea1130885ee09ffdc4b1
Vivek Singh Solanki <viveksolanki0509@gmail.com>
2026-07-31 21:46:08 +0530

    Extract plugin-api security module (#10941)
    * Extract plugin-api security module
    
    Move the five security module contracts into a new crypto/plugin-api module, published as besu-plugin-api-security and re-exported by besu-plugin-api (japicmp-verified unchanged surface). Relocate SecurityModuleServiceImpl into crypto/services' new pluginadapter package.
    
    Signed-off-by: Vivek Singh Solanki <viveksolanki0509@gmail.com>
    
    * Add changelog entry for the security module extraction
    
    Signed-off-by: Vivek Singh Solanki <viveksolanki0509@gmail.com>
    
    ---------
    
    Signed-off-by: Vivek Singh Solanki <viveksolanki0509@gmail.com>

--- crypto/services/src/main/java/org/hyperledger/besu/cryptoservices/pluginadapter/SecurityModuleServiceImpl.java
+++ crypto/services/src/main/java/org/hyperledger/besu/cryptoservices/pluginadapter/SecurityModuleServiceImpl.java
@@ -12,7 +12,7 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-package org.hyperledger.besu.services;
+package org.hyperledger.besu.cryptoservices.pluginadapter;
 
 import org.hyperledger.besu.plugin.services.SecurityModuleService;
 import org.hyperledger.besu.plugin.services.securitymodule.SecurityModule;
